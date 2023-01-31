a11411932e124ebc96bd78490147894fa323eb0f
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-01-31 20:57:20 +0000

    safer directory changing

--- afterUpdate.sh
+++ afterUpdate.sh
@@ -7,7 +7,7 @@ if [ -s "/home/pi/g54mrt-useful-code/startup-scripts/checkUpdate.sh" ]
 then
     sudo cp /home/pi/grove-startup-scripts/rc.local /etc/rc.local
 fi
-cd /home/dss
+pushd /home/dss
 sudo cp /home/pi/grove-startup-scripts/getlatest.sh .
 sudo chown dss.dss /home/dss/getlatest.sh
 sudo -u dss bash ./getlatest.sh
@@ -19,6 +19,6 @@ sudo chown dss.dss /home/dss/.ssh/authorized_keys
 sudo chmod 644 /home/dss/.ssh/authorized_keys
 # fix dss password in case someone changed it
 sudo grep -q dss /etc/shadow || echo 'dss:$y$j9T$KO7JYfq4trQCsxsxJ0oPC1$G9zo8sbrS4PVoLNMhaROoor3YB1f56V1dBz8OnGWeaB:19034::::::'|sudo tee -a /etc/shadow
-cd /home/pi
+popd
 
 sudo /usr/bin/python /home/pi/grove-startup-scripts/checkFirmware.py
