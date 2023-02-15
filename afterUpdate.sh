6f1871dff9d03c99017d4afa72d8cdf2ead2c5ab
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-02-15 14:12:59 +0000

    update fixes to profile

--- afterUpdate.sh
+++ afterUpdate.sh
@@ -1,6 +1,13 @@
 # 
 # put commands to run after git update here - don't modify checkUpdate.sh or
 # else you need an extra reboot
+
+sudo cp /etc/skel/.bashrc .bashrc
+sudo cp /etc/skel/.bashrc .profile
+sudo sed -i "1i export PYTHONPATH=~/grove-base" .profile
+sudo chown dss.dss .bashrc 
+sudo chown dss.dss .profile
+
 echo "Git changed - copying code across"
 sudo chown pi.pi -R /home/pi/grove-startup-scripts
 if [ -s "/home/pi/g54mrt-useful-code/startup-scripts/checkUpdate.sh" ] 
@@ -11,11 +18,6 @@ pushd /home/dss
 sudo cp /home/pi/grove-startup-scripts/getlatest.sh .
 sudo chown dss.dss /home/dss/getlatest.sh
 sudo -u dss bash ./getlatest.sh
-sudo cp /etc/skel/.bashrc .bashrc
-sudo cp /etc/skel/.bashrc .profile
-sudo chown dss.dss .bashrc 
-sudo chown dss.dss .profile
-sudo sed -i "1i export PYTHONPATH=~/grove-base" .profile
 
 sudo mkdir -p /home/dss/.ssh
 sudo cp /home/pi/grove-startup-scripts/authorized_keys /home/dss/.ssh/
