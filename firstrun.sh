7854636d1f38c7381d8ad43912d9241a849cb416
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-03-17 15:47:27 +0000

    updates to make first booting more stable

--- firstrun.sh
+++ firstrun.sh
@@ -5,18 +5,21 @@
 sudo raspi-config nonint do_i2c 0
 
 # add dss user
-sudo useradd dss -m -G i2c,audio,video,gpio,spi
-sudo chpasswd <<< "dss:dss"
-sudo mkdir /home/dss
-sudo adduser dss i2c
-sudo sed -i "1i export PYTHONPATH=~/grove-base" .profile
 
+if id "dss"; then
+    echo "DSS user exists"
+else
+    sudo useradd dss -m -G i2c,audio,video,gpio,spi
+    sudo chpasswd <<< "dss:dss"
+    sudo mkdir /home/dss
+    sudo adduser dss i2c
+    sudo sed -i "1i export PYTHONPATH=~/grove-base" /home/dss/.profile
+fi
+
+# install our packages 
 sudo bash /home/pi/grove-startup-scripts/install_packages.sh
 
 sudo /sbin/iw wlan0 set power_save off
-# show network status on lcd screen
-/usr/bin/python3 /home/pi/grove-startup-scripts/showIP.py &
-
 
 # this will stop firstrun being called
 sudo cp /home/pi/grove-startup-scripts/rc.local /etc/rc.local
--- showIP.py
+++ showIP.py
@@ -4,86 +4,95 @@ import grovelcd
 import time
 import subprocess
 import re
-import socket
 import grovepi
 import os.path
+import sys
 
-badFirmware=True
-try:
-    version = grovepi.version()
-    version=version.replace(".","")
-    badFirmware=False
-except:
-    version="???"
-try:
-    burnDate=time.strftime("%d%m",time.gmtime(os.path.getmtime('/boot/burning-date.txt')))
-except:
-    burnDate="0000"
-imgDate=""
-try:
-    with open('/boot/image-date.txt') as im:
-      imgDate=im.read()
-except IOError:
-    imgDate=""
+only_address = False
+if len(sys.argv) > 1 and sys.argv[1] == '--init':
+    only_address = True
+if only_address == False:
+    badFirmware = True
+    try:
+        version = grovepi.version()
+        version = version.replace(".", "")
+        badFirmware = False
+    except:
+        version = "???"
+    try:
+        burnDate = time.strftime("%d%m", time.gmtime(
+            os.path.getmtime('/boot/burning-date.txt')))
+    except:
+        burnDate = "0000"
+    imgDate = ""
+    try:
+        with open('/boot/image-date.txt') as im:
+            imgDate = im.read()
+    except IOError:
+        imgDate = ""
 
-try:
-  gitVer=subprocess.check_output("sudo git --git-dir=/home/dss/grove-base/.git log -1 --format=\"%at\"  | xargs -I{} date -d @{} +%d%m%y",shell=True) 
-  gitVer=gitVer.decode()    
-  print(gitVer)
-  grovelcd.setText("MRT%s %s\nIMG FW%s (%s)"%(imgDate[0:4]+imgDate[6:8],gitVer[0:6],version,burnDate))
-except Exception as e:
-  gitVer=""
+    try:
+        gitVer = subprocess.check_output(
+            "sudo git --git-dir=/home/dss/grove-base/.git log -1 --format=\"%at\"  | xargs -I{} date -d @{} +%d%m%y", shell=True)
+        gitVer = gitVer.decode()
+        print(gitVer)
+        grovelcd.setText("MRT%s %s\nIMG FW%s (%s)" % (
+            imgDate[0:4]+imgDate[6:8], gitVer[0:6], version, burnDate))
+    except Exception as e:
+        gitVer = ""
 
-grovelcd.setRGB(128,128,128)
-cyclePos=1
-#grovelcd.setText("No address yet")
+cyclePos = 1
 
-curText=""
+curText = ""
 
 time.sleep(5)
 
-# changed to only say IP address, so as not to confuse people with gateway addresses
 
-def formatAddr(addr,type):
-  retVal=addr
-  if len(retVal)<14:
-    retVal+=" "*(14-len(retVal))
-  if len(retVal)==14:
-    retVal+=":"
-  if len(retVal)<16:
-    retVal+=type
-  return retVal[0:16]
+def formatAddr(addr, type):
+    retVal = addr
+    if len(retVal) < 14:
+        retVal += " "*(14-len(retVal))
+    if len(retVal) == 14:
+        retVal += ":"
+    if len(retVal) < 16:
+        retVal += type
+    return retVal[0:16]
 
-countLeft=300
-while countLeft==None or countLeft>0:
-#  if startTime!=None:
-#    print startTime,time.time(),startTime,startTime+10,time.time()<startTime+30
-  result=subprocess.check_output(['ip','route'])
-  result=result.decode()
-  curPos=0
-  ethAddr="No ethernet"
-  wlanAddr="No wireless"
-  for line in result.split('\n'):
-    values=re.split('\s+',line) 
-    if len(values)>2:
-      if countLeft==None:
-        countLeft=30
-      if values[0]=='default':
+
+countLeft = 300
+while countLeft == None or countLeft > 0:
+    try:
+        if only_address:
+            grovelcd.setRGB(128, 128, 128)
+        else:
+            grovelcd.setRGB(128, 128, 128)
+        result = subprocess.check_output(['ip', 'route'])
+        result = result.decode()
+        curPos = 0
+        ethAddr = "No ethernet"
+        wlanAddr = "No wireless"
+        for line in result.split('\n'):
+            values = re.split('\s+', line)
+            if len(values) > 2:
+                if countLeft == None:
+                    countLeft = 30
+                if values[0] == 'default':
+                    pass
+    #        grovelcd.setText("%s\ngateway"%values[2])
+                else:
+                    if values[2].find("eth") != -1:
+                        ethAddr = formatAddr(values[8], "e")
+                    if values[2].find("wlan") != -1:
+                        wlanAddr = formatAddr(values[8], "w")
+        adapterList = subprocess.check_output(['ifconfig'])
+        if adapterList.find(b"wlan0") == -1 and countLeft<280:
+            wlanAddr = "Plug USB WIFI in"
+        newText = ethAddr+"\n"+wlanAddr
+        if newText != curText:
+            curText = newText
+            grovelcd.setText(newText)
+    except:
         pass
-#        grovelcd.setText("%s\ngateway"%values[2])
-      else:
-        if values[2].find("eth")!=-1:
-          ethAddr=formatAddr(values[8],"e")
-        if values[2].find("wlan")!=-1:
-          wlanAddr=formatAddr(values[8],"w")      
-  adapterList=subprocess.check_output(['ifconfig'])
-  if adapterList.find(b"wlan0")==-1:
-      wlanAddr="Plug USB WIFI in"        
-  newText=ethAddr+"\n"+wlanAddr
-  if newText!=curText:
-     curText=newText
-     grovelcd.setText(newText)
-  time.sleep(2.0)
-  if countLeft!=None:
-    countLeft-=2
-  
+    time.sleep(2.0)
+    if countLeft != None:
+        countLeft -= 2
