94d2ce82545cba8557e0ed2d13b43ff855420b37
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-03-17 13:07:18 +0000

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -11,42 +11,12 @@ sudo mkdir /home/dss
 sudo adduser dss i2c
 sudo sed -i "1i export PYTHONPATH=~/grove-base" .profile
 
+sudo bash ./install_packages.sh
 
 sudo /sbin/iw wlan0 set power_save off
 # show network status on lcd screen
 /usr/bin/python3 /home/pi/grove-startup-scripts/showIP.py &
 
-NET=0
-
-# wait for network
-while true
-do
-    NET=1
-    wget -w3 -O/dev/null https://www.github.com  && break
-    sudo iwlist wlan0 scan > /dev/null
-    NET=0
-    sleep 1
-done
-
-while true
-do
-    sudo apt-get update -y
-    sudo apt-get install -y /home/pi/grove-startup-scripts/avrdude_6.2-2_armhf.deb
-    sudo apt-get install -y screen
-    sudo apt-get install -y libncurses5
-    sudo apt-get install -y libftdi1
-    sudo apt-get install -y subversion
-    # make sure libatlas is installed so numpy works
-    sudo apt-get install -y --no-upgrade libatlas-base-dev
-    # install tensorflow lite 2.11.0
-    sudo python3 -m pip install tflite-runtime=2.11.0
-    sudo apt-get install -y git
-
-    # if git is broken, remove apt-get lists and update again
-    git --version && break 
-    
-    sudo rm /var/lib/apt/lists/*
- done
 
 # this will stop firstrun being called
 sudo cp /home/pi/grove-startup-scripts/rc.local /etc/rc.local
