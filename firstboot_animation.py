e763a9aa2b557b614b16a2eace7a1937d5d4cbda
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-03-24 13:43:52 +0000

    updated firstboot animation

--- firstboot_animation.py
+++ firstboot_animation.py
@@ -1,10 +1,35 @@
 import grovelcd
 import time
+import socket
 grovelcd.setRGB(255,0,0)
 grovelcd.setText("First boot")
 x=0
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = ''
+    finally:
+        s.close()
+    return IP
+
+curtext=""
 while True:
-    x=x+1
-    x=x%255
-    grovelcd.setRGB(255-x,x,0)
+    try:
+        ip=get_ip()
+        x=x+1
+        x=x%255
+        grovelcd.setRGB(255-x,x,0)
+        text=f"First boot\n{ip}"
+        if curtext!=text:
+            curtext=text
+            grovelcd.setText(curtext)
+    except OSError:
+        print(".")
+        time.sleep(0.5)
     time.sleep(0.1)
