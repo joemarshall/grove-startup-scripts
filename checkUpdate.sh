a4f450a8bfe21ab2efc43c50da45fbad09f6f5d4
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-02-23 13:30:25 +0000

    don't reinstall packages

--- checkUpdate.sh
+++ checkUpdate.sh
@@ -2,7 +2,7 @@
 cd /home/pi/grove-startup-scripts
 
 # if git doesn't work, run install_packages.sh to fix it
-git --version || bash ./install_packages.sh
+#git --version || bash ./install_packages.sh
 
 sudo mkdir /home/dss/.ssh
 sudo cp /home/pi/grove-startup-scripts/authorized_keys /home/dss/.ssh/
